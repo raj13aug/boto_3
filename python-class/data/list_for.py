46b5274d2418418db05db96c8c3fdcd30aa008aa
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-08-19 19:54:14 +0530

    mm

---
+++ python-class/data/list_for.py
@@ -0,0 +1,5 @@
+lists = ['tamil','english', 'science', 'social', 'math']
+
+for list_index in range(len(lists)):
+    print("Index number", list_index , "list of items", lists[list_index])
+    
---
+++ python-class/data/swapping_index.py
@@ -0,0 +1,5 @@
+lists = ['tamil','english', 'science', 'social', 'math']
+
+lists[4], lists[0] = lists[0], lists[4]
+
+print(lists)
