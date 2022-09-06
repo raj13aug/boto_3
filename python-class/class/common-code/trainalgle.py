919fb707e422c73fc0a594223fe86cfaab5402fb
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-06 20:38:02 +0530

    Create trainalgle.py

---
+++ python-class/class/common-code/trainalgle.py
@@ -0,0 +1,16 @@
+
+'''
+pass  1- 1 2 3 4 5
+pass  2- 1 2 3 4 5
+pass  3- 1 2 3 4 5
+pass  4- 1 2 3 4 5
+pass  5- 1 2 3 4 5
+
+'''
+
+for x in range(1,6):
+    print("pass", x,"-",end='')
+    for y in range(1,6):
+        print(y, end='')
+    print()
+    
