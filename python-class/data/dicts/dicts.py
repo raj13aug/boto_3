ace3dfae3e9589376df040061830d8e838d51e86
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-06 17:11:18 +0530

    Update dicts.py

--- python-class/data/dicts/dicts.py
+++ python-class/data/dicts/dicts.py
@@ -42,3 +42,82 @@ animals = {
 }
 
 print(animals[2])
+
+
+# we assign the cannot assign the list value in key 
+
+# animals = {
+#     [1] : 'dog',
+#     [2] : 'cat',
+#     [3] : 'cow'
+# }
+
+# print(animals[2])
+
+
+# assign the string into key
+
+city_rating = {
+    'banglore' : [7,8],
+    'chennai'  : [7,8]
+}
+
+
+# assig the value to dictiories 
+
+grades = {}
+
+grades['John'] = 'A'
+grades['Ram'] = 'B'
+
+print(grades)
+
+
+# update the value 
+
+grades.update({'John' : '-A'})
+print(grades)
+
+# count the dictionaries value
+
+print(len(grades))
+
+# if condition 
+
+for John in grades:
+    print( "John got", grades['John'])
+    
+    
+# del the value    
+
+del(grades['John'])
+print(grades)
+
+
+# for loop and you get the key value
+
+grades = {}
+
+grades['bye'] = 'A'
+grades['hello'] = 'B'
+
+for el in grades:
+    print(el)
+    
+    
+# or both are similar output.
+
+for el in grades.keys():
+    print(el)
+    
+    
+#using for loop get the dictionries value
+
+for el in grades.values():
+    print(el)
+    
+    
+# Get the both value in dictionaries
+
+for x,y in grades.items():
+    print(x ,'got', y)
