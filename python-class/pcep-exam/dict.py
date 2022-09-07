64a7faa705d2e34f34c7e46731a8b189679aa01b
Nataraj Ramamoorthy <nataraj.ramamoorthy85@gmail.com>
2022-09-07 20:27:05 +0530

    7-sep-2022

---
+++ python-class/pcep-exam/dict.py
@@ -0,0 +1,3 @@
+d1 = {"sandy":35, "surname":"Kate"}
+d2 = {"surname":"Kate", "sandy":35}
+print(d1 == d2)
---
+++ python-class/pcep-exam/while.py
@@ -0,0 +1,5 @@
+data = ""
+while data:
+   print("Data is not Empty")
+else:
+   print("Data is Empty")
